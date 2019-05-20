2ab642740f18a457acf05661b697a8888408dc36
jacob <jacob_a_reed@yahoo.com>
2019-05-20 15:58:21 +0000

    add a route called admin

---
+++ routes/admin.js
@@ -0,0 +1,16 @@
+//dashboard
+
+//<li>Dashboard - view visitors, products bought</li>
+
+var visitors = require('./models/visitors');
+
+var transactions = require('./models/transactions');
+
+//count how many visitors today
+
+//return last ten visitors
+
+//count how many transactions happened today
+
+//return how many transactions of each product
+
