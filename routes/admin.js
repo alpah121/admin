e380173467ca0ae740842082e147853ed786729b
jacob <jacob_a_reed@yahoo.com>
2019-05-21 08:45:26 +0000

    added dashboard routes

--- routes/admin.js
+++ routes/admin.js
@@ -1,16 +1,33 @@
 //dashboard
-
-//<li>Dashboard - view visitors, products bought</li>
+admin = {};
+//<li>Dashboard - view visitors, products bought, current(todays) gross sales</li>
 
 var visitors = require('./models/visitors');
 
 var transactions = require('./models/transactions');
 
+function getToday()
+{
+const now = new Date();
+const today = new Date(now.getFullYear(), now.getFullMonth, now.getDate());
+return today;
+}
+
 //count how many visitors today
-
+admin.visitorsToday = (req, res) => {visitors.find({created: getToday()})
+.count((count) => {res.send(count);})
+.catch((error) => {res.status(500);});
+};
 //return last ten visitors
+admin.lastTenVisitors = (req, res) => {visitors.find({created: getToday()})
+.limit(10)
+.exec((visitors) => {res.json(visitors);})
+.catch((error) => {res.status(500);});	
+};
 
-//count how many transactions happened today
-
-//return how many transactions of each product
+//return all transactions created today
+admin.transactionsToday = (req, res) => {transactions.find({created: getToday()})
+.exec((transactions) => {res.json(transactions);})
+.catch((error) => {res.status(500);});	
+};
 
